0948cd16f5e212e74beebe23780c72347bce07c9
푸딩비 <91423376+chamypuppy@users.noreply.github.com>
2023-04-21 16:39:06 +0900

    코딩애플 | useNavigate()와 Nested Routes
    ㅠㅠ 방금 전에 커밋하였는데 src 폴더가 아니라서 업로드가 되지 않았다ㅠ.ㅠ
    
    그래서 아래 링크에 이번 코드 관련하여 <리액트 라우트>에 대해 자세하게 정리해놓은 나의 블로그 링크를 첨부한다!😊
    https://chamypuppy.tistory.com/71

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import bgImg from "./img/bg.png";
 import shoes1 from "./img/shoes1.jpg";
 import shoes2 from "./img/shoes2.jpg";
 import shoes3 from "./img/shoes3.jpg";
-import { Navbar, Container, NavDropdown, Nav, Row, Col } from "react-bootstrap";
+import {
+  Navbar,
+  Container,
+  NavDropdown,
+  Nav,
+  Row,
+  Col,
+  AccordionButton,
+} from "react-bootstrap";
 import { useState } from "react";
 import data from "./data.js";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import Detail from "./Pages/Detail.js";
 
 function App() {
   let [shoes] = useState(data);
   let [colImg] = useState(shoes1);
   let [num] = useState(0);
   let [numArray] = useState([0, 1, 2]);
+  let navigate = useNavigate();
 
   return (
     <div className="App">
@@ -22,8 +32,28 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Link to="/">홈</Link>
-              <Link to="/detail">상세페이지</Link>
+              <Nav.Link
+                onClick={() => {
+                  navigate("/");
+                }}
+              >
+                {" "}
+                홈{" "}
+              </Nav.Link>
+              <Nav.Link
+                onClick={() => {
+                  navigate("/detail");
+                }}
+              >
+                상세페이지
+              </Nav.Link>
+              <Nav.Link
+                onClick={() => {
+                  navigate("/event");
+                }}
+              >
+                이벤트
+              </Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
@@ -42,8 +72,8 @@ function App() {
         </Container>
       </Navbar>
 
-      <Link to="/">홈</Link>
-      <Link to="/detail">상세페이지</Link>
+      {/* <Link to="/">홈</Link> */}
+      {/* <Link to="/detail">상세페이지</Link> */}
 
       <Routes>
         <Route
@@ -59,7 +89,13 @@ function App() {
                 <Row>
                   {numArray.map(function (a, i) {
                     return (
-                      <Columns shoes={shoes} colImg={colImg} num={num} i={i} />
+                      <Columns
+                        shoes={shoes}
+                        colImg={colImg}
+                        num={num}
+                        i={i}
+                        key={i}
+                      />
                     );
                   })}
                 </Row>
@@ -69,7 +105,34 @@ function App() {
         />{" "}
         {/* 메인페이지 */}
         <Route path="/detail" element={<Detail />} />
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>회사 멤버스</div>} />
+          <Route path="location" element={<div>회사 위치</div>} />
+        </Route>
+        <Route path="/event" element={<Event />}>
+          <Route path="one" element={ <div>첫 주문시 양배추즙 서비스</div>} />
+          <Route path="two" element={ <div>생일기념 쿠폰받기</div>} />
+         </Route>
+        <Route path="*" element={<div>없는 페이지입니다</div>} />
       </Routes>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h4>회사 정보</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+function Event() {
+  return (
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
     </div>
   );
 }
@@ -81,27 +144,6 @@ function Columns(props) {
       <h4>{props.shoes[props.i].title}</h4>
       <p>{props.shoes[props.i].price}</p>
     </Col>
-  );
-}
-
-function Detail() {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-6">
-          <img
-            src="https://codingapple1.github.io/shop/shoes1.jpg"
-            width="100%"
-          />
-        </div>
-        <div className="col-md-6">
-          <h4 className="pt-5">상품명</h4>
-          <p>상품설명</p>
-          <p>120000원</p>
-          <button className="btn btn-danger">주문하기</button>
-        </div>
-      </div>
-    </div>
   );
 }
 
