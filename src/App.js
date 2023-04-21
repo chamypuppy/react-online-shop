48b6db932b27636bd1f54f2058b808963a6b62c2
푸딩비 <91423376+chamypuppy@users.noreply.github.com>
2023-04-21 18:20:28 +0900

    코딩애플 | 이미 서식있는 상세페이지 여러 개 만들기 url 파라미터와 useParam()
    ★ /: 작명
        
         예) <Route path="/detail/:id" element={<Detail />} />        -> id 라고 작명 
        주소창에 상세 페이지 관련 라우터 이름 다음에 
        슬래시 + 콜론 + 아무 글자  입력 시 모두 해당 상세 페이지 라우터의 element 속성 안의 대문자 컴포넌트의 레이아웃으로 보여달라고 요청하는 개념이다.
        '예)'를 기준으로 사용자가 detail/아무거나 입력 시, <Detail /> 레이아웃을 보여줄 수 있게 하는 개념이다.
        이때 모두 같은 페이지를 보여주게 된다.
        => 이를 url 파라미터라 칭한다.
        => url 파라미터는 사용자가 상위 라우터/작명 자리에 입력한 값이 된다.
    
    🔺 라우트를 여러 개 만들면 안 되나요?
          안 되는 것은 아니다.
          하지만 100만개 이상의 상세 페이지가 존재하는 경우, 수정 사항 발생 시 대응이 어렵다.
          => 즉 유지 보수에 좋은 방법이 아니다.
    
    ✔ 참고로 url 파라미터는 (한 라우터에서) 몇 번이고 사용할 수 있다.
          예) path="/detail/:lala/어쩌구/:lalala"
          => 일반 라우터와 번갈아가며 사용할 수도 있다.
    
    🔺 모두 같은 상세 페이지만 보이는 걸 개선하고 싶어요
          ★ useParam()
          => url 파라미터 값을,
               그러니까 사용자가 url 주소창에 입력한 url 파라미터 값을 가져오게 하는 함수!
          => 변수에 저장해서 사용하면 된다!
    
         ✔ useParam() 변수 저장 시
               =>   let {  } = useParam( );
              으로 저장하면 된다. 대괄호 안에는 아까 url 파라미터 작명했던 이름으로 기입하면 된다.
              변수 저장 시 대괄호를 쓰는 이유는 내가 콘솔 로그에서 확인해보았을 때
              a.   변수 이름에 { id }로  => 사용자가 detail/3333 입력 => (콘솔 로그창) 3333
              b.   변수 이름에 { } 없음  => 사용자가 detail/3333 입력 => (콘솔 로그창)  { 변수명 : '3333' }
             이렇게 다르게 뜨는 것을 보고 대략적으로 알 수 있었다.

--- src/App.js
+++ src/App.js
@@ -28,7 +28,10 @@ function App() {
     <div className="App">
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">푸딩 쇼핑몰</Navbar.Brand>
+          <Navbar.Brand href="#home"
+            onClick={() => { navigate("/") }}>
+            푸딩 쇼핑몰
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
@@ -104,15 +107,15 @@ function App() {
           }
         />{" "}
         {/* 메인페이지 */}
-        <Route path="/detail" element={<Detail />} />
+        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
         <Route path="/about" element={<About />}>
           <Route path="member" element={<div>회사 멤버스</div>} />
           <Route path="location" element={<div>회사 위치</div>} />
         </Route>
         <Route path="/event" element={<Event />}>
-          <Route path="one" element={ <div>첫 주문시 양배추즙 서비스</div>} />
-          <Route path="two" element={ <div>생일기념 쿠폰받기</div>} />
-         </Route>
+          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>} />
+          <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
+        </Route>
         <Route path="*" element={<div>없는 페이지입니다</div>} />
       </Routes>
     </div>
--- src/Pages/Detail.js
+++ src/Pages/Detail.js
@@ -1,4 +1,10 @@
-function Detail() {
+import { useParams } from "react-router-dom";
+
+function Detail(props) {
+
+  let { id } = useParams();
+  console.log(id);
+
   return (
     <div className="container">
       <div className="row">
@@ -9,9 +15,9 @@ function Detail() {
           />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">상품명</h4>
-          <p>상품설명</p>
-          <p>120000원</p>
+          <h4 className="pt-5"> {props.shoes[id].title} </h4>
+          <p>{props.shoes[id].content}</p>
+          <p>{props.shoes[id].price}원</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
